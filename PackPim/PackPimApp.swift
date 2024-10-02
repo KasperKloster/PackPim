577538d556abeae1d466450fb145b4ef9c4e9293
Kasper Kloster <kasperkloster@gmail.com>
2024-10-02 20:44:05 +0200

    Initial Commit

---
+++ PackPim/PackPimApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackPimApp.swift
+//  PackPim
+//
+//  Created by Kasper Kloster on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PackPimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
