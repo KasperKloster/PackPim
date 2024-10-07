52e88f6fa4fde92839c943bde29c950029296c44
KasperKloster <kasperkloster@gmail.com>
2024-10-07 18:37:24 +0200

    Integrations further setup

---
+++ PackPim/Features/Integrations/Create/CreateIntegrationView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateIntegrationView.swift
+//  PackPim
+//
+//  Created by Kasper Kloster on 06/10/2024.
+//
+
+import SwiftUI
+
+struct CreateIntegrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateIntegrationView()
+}
---
+++ PackPim/Features/Integrations/IntegrationRouterProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  IntegrationRouterProtocol.swift
+//  PackPim
+//
+//  Created by Kasper Kloster on 06/10/2024.
+//
+
+import Foundation
