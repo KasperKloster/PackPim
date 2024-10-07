840d9c465d2653accfac525ce854086fa417863c
KasperKloster <kasperkloster@gmail.com>
2024-10-07 21:14:01 +0200

    Tests init

---
+++ PackPimTests/TestFeatures/TestProducts/TestProductEntity.swift
@@ -0,0 +1,16 @@
+//
+//  TestProductEntity.swift
+//  PackPim
+//
+//  Created by Kasper Kloster on 07/10/2024.
+//
+
+import Testing
+
+struct TestProductEntity {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
