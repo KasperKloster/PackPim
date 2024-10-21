7215c3c25a0a7718c64784ff3769c48529e70e2f
KasperKloster <kasperkloster@gmail.com>
2024-10-21 23:14:56 +0200

    Better folders. Read Integration init.

---
+++ PackPim/Features/Integrations/Read/View/ReadIntegration.swift
@@ -0,0 +1,35 @@
+//
+//  ReadIntegration.swift
+//  PackPim
+//
+//  Created by Kasper Kloster on 21/10/2024.
+//
+
+import SwiftUI
+
+struct ReadIntegration: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    customHeadLineText("Integration")
+                    Text("Use upload products to import a csv")
+                    Text("Click sync to sync with API")
+                }
+                Spacer()
+                Button("Upload products", systemImage: "square.and.arrow.up", action: actionBtn)
+                Button("Sync products", systemImage: "arrow.trianglehead.2.clockwise.rotate.90", action: actionBtn)
+            }
+            
+        }
+        .padding()
+    }
+}
+
+func actionBtn(){
+    return;
+}
+
+#Preview {
+    ReadIntegration()
+}
